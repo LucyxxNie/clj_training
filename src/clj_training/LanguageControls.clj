824692ff95e4f314c1f7b86b1d301abd71d9e89f
LucyxxNie <nie103578@outlook.com>
2023-04-27 10:58:57 -0400

    none

--- src/clj_training/LanguageControls.clj
+++ src/clj_training/LanguageControls.clj
@@ -19,3 +19,22 @@
 (#(+ % 1) 2)
 
 ;vars
+
+; symbol -> var -> function
+
+(def cat 5)
+(def dog 10)
+(def dogName ["Oreo", "Liz"])
+
+;!!redefine vars in clj is dangerous! because changing name everywhere, including other functions running with it!!!
+(def var 5)
+(def var 6)                                                 ;!!!DO NOT redefine THIS!
+
+;defining function
+
+;below 2 are the same, defn is short for def + fn
+(def half (fn [num] (/ num 2)))
+(defn half2 [num] (/ num 2))
+
+(defn catStr [] "I have cat")
+
