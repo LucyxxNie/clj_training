d4c71256024d4e0f1d66a61407c65dfe6415463b
LucyxxNie <nie103578@outlook.com>
2023-05-08 12:09:04 -0400

    none

--- src/aoc2020/day3.clj
+++ src/aoc2020/day3.clj
@@ -2,17 +2,18 @@
   (:require [aoc2020.util :refer [file->seq]]))
 
 
-(defn tob-moving-slope-row
-  [row-size]
-  (range 1 row-size))
+(defn tob-slope-down
+  [row-size down-slp]
+  (rest (range 0 row-size down-slp)))
 
-(defn tob-moving-slope-col
-  [col-size row-size]
-  (->> (iterate (fn [col]
-                  (-> col
-                    (+ 3)
-                    (mod col-size))) 3)
-    (take (- row-size 1))))
+(defn tob-slope-right
+  [col-size right-slp row-size down-slp]
+  (let [num-of-stops (- row-size down-slp)]
+    (->> (iterate (fn [col]
+                    (-> col
+                      (+ right-slp)
+                      (mod col-size))) right-slp)
+      (take num-of-stops))))
 
 (defn tree-enctr?
   [cur-row cur-col map-s]
@@ -24,16 +25,25 @@
       false)))
 
 (defn total-tree-enctr-cnt
-  [map-s]
+  [map-s & {:keys [right-slp down-slp]}]
   (let [col-size (count (get map-s 1))
         row-size (count map-s)]
     (->> (mapv (fn [cur-row cur-col]
                  (tree-enctr? cur-row cur-col map-s))
-           (tob-moving-slope-row row-size)
-           (tob-moving-slope-col col-size row-size))
+           (tob-slope-down row-size down-slp)
+           (tob-slope-right col-size right-slp row-size down-slp))
       (remove false?)
       (count))))
 
+(defn tree-enctr-multiply
+  [map-s]
+  (->> (vector
+         (total-tree-enctr-cnt map-s :right-slp 1 :down-slp 1)
+         (total-tree-enctr-cnt map-s :right-slp 3 :down-slp 1)
+         (total-tree-enctr-cnt map-s :right-slp 5 :down-slp 1)
+         (total-tree-enctr-cnt map-s :right-slp 7 :down-slp 1)
+         (total-tree-enctr-cnt map-s :right-slp 1 :down-slp 2))
+    (reduce *)))
 
 
 
@@ -67,23 +77,17 @@
         "...#....##...#...#...#.#..#....",,,]
 
   ;;------------------------------function eval part 1------------------------------------------
+  ;;tobbogan moving at slope right 3 down 1, calculate total tree encounter
 
-  (def example ["..#.#.#"
-                ".#.##.#"
-                "...####"])
-
-  (tob-moving-slope-row 5)
+  (tob-slope-down 5 1)
   #_=> (1 2 3 4)
-  (tob-moving-slope-row 10)
-  #_=> (1 2 3 4 5 6 7 8 9)
+  (tob-slope-down 5 2)
+  #_=> (2 4)
 
-  (tob-moving-slope-col 5 5)
-  #_=> (3 1 4 2)
-  (tob-moving-slope-col 10 10)
-  #_=> (3 6 9 2 5 8 1 4 7)
-  (tob-moving-slope-col 15 7)
-  #_=> (3 6 9 12 0 3)
-
+  (tob-slope-right 10 3 5 1)
+  #_=> (3 6 9 2)
+  (tob-slope-right 15 7 5 2)
+  #_=> (7 14 6)
 
   (tree-enctr? 1 3 ["..#.#.#"
                     ".#.##.#"
@@ -96,14 +100,29 @@
 
   (total-tree-enctr-cnt ["..#.#.#"
                          ".#.##.#"
-                         "...###."])
+                         "...###."], :right-slp 3, :down-slp 1)
   #_=> 1
 
 
-  (total-tree-enctr-cnt sample-map)
+  (total-tree-enctr-cnt sample-map, :right-slp 3, :down-slp 1)
   #_=> 7
 
-  (total-tree-enctr-cnt entry-map)
+  (total-tree-enctr-cnt entry-map, :right-slp 3, :down-slp 1)
   #_=> 220
+
+
+  ;;-----------------------function eval part 2----------------------------------
+  ;;calculate the product by multiplying the tree encounter using different slp
+
+  (tree-enctr-multiply ["###.#.#"
+                        ".####.#"
+                        "######."])
+  #_=> 2
+
+  (tree-enctr-multiply sample-map-s)
+  #_=> 336
+
+  (tree-enctr-multiply entry-map-s)
+  #_=> 2138320800
 
   )
